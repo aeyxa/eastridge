43d39ddbc3104a11173743d01801fbd2e2c20199
aeyxa <kianedits@hotmail.com>
2017-09-19 15:36:34 -0700

    probably done maybe possibly

--- app.py
+++ app.py
@@ -2,26 +2,90 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from classes import Invoice, InvoiceItem
 
 @view_config(route_name='home')
 def home(request):
-    return Response
-    ("""
-        <form action="/test" method="post">
-            <input id="invoice" name="invoice" value=""/><br>
-            <input type="submit" value="submit">
-        </form>
+    return Response("""
+        <html lang="en">
+            <head></head>
+            <body>
+                <div>
+                    <form action="/invoice" method="POST">
+                        <button type="submit">Create</button
+                    </form>
+                </div>
+            </body>
+        </html>
     """)
 
-@view_config(route_name='test', request_method='POST')
-def test(request):
-    return Response('%s' % request.params['invoice'])
+@view_config(route_name='invoice', request_method='POST')
+def invoice(request):
+    return HTTPFound(location='/invoice/%s' % Invoice.create())
+
+@view_config(route_name='invoice_item')
+def invoice_item(request):
+    current_id = request.url.split('/')[-1]
+
+    if(Invoice.find(current_id) is None):
+        return HTTPFound(location='/')
+
+    invoice_items = {}
+    for item in InvoiceItem.find(current_id):
+        invoice_items['units_%s' % item.id] = item.units
+        invoice_items['description_%s' % item.id] = item.description
+        invoice_items['amount_%s' % item.id] = item.amount
+
+    return Response("""
+        <html>
+            <head></head>
+            <body>
+                <div>
+                    <form action="/invoice_item/create/%s" method="POST">
+                        <label for="units">units</label>
+                        <input type="number" id="units" name="units"/>
+
+                        <label for="description">description</label>
+                        <input type="text" id="description" name="description"/>
+
+                        <label for="amount">amount</label>
+                        <input type="number" id="amount" name="amount"/>
+
+                        <button type="submit">Submit</button>
+                    </form>
+
+                    <div>
+                        %s
+                    </div>
+                    <br>
+                    <div>
+                        <a href="/">create a new invoice</a>
+                    </div>
+                </div>
+            </body>
+        </html>
+    """ % (current_id, invoice_items))
+
+@view_config(route_name='invoice_item_create', request_method='POST')
+def invoice_item_create(request):
+
+    invoice_id = request.url.split('/')[-1]
+    units = request.params['units']
+    description = request.params['description']
+    amount = request.params['amount']
+
+    invoice_item = InvoiceItem.create(invoice_id,units,description,amount)
+
+    return HTTPFound(location='/invoice/%s' % invoice_id)
 
 if __name__ == '__main__':
     with Configurator() as config:
         config.add_route('home', '/')
-        config.add_route('test', '/test')
+        config.add_route('invoice', '/invoice')
+        config.add_route('invoice_item','/invoice/{id}')
+        config.add_route('invoice_item_create', '/invoice_item/create/{id}')
         config.scan()
         app = config.make_wsgi_app()
-    server = make_server('216.98.139.16', 8080, app)
+    server = make_server('216.98.139.16', 80, app)
     server.serve_forever()
---
+++ classes.py
@@ -0,0 +1,80 @@
+import datetime
+from sqlalchemy import *
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Invoice(Base):
+
+    __tablename__ = 'invoices'
+
+    id = Column(Integer, primary_key=True, unique=True)
+    date = Column(DateTime, default=datetime.datetime.utcnow)
+    invoice_items = relationship('InvoiceItem',backref='invoice',lazy='dynamic')
+
+    def create():
+
+        engine = create_engine('mysql://root@localhost/test')
+        Base.metadata.create_all(bind=engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        invoice = Invoice()
+        session.add(invoice)
+        session.commit()
+
+        return invoice.id
+
+    def find(id):
+
+        engine = create_engine('mysql://root@localhost/test')
+        Base.metadata.create_all(bind=engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        invoice = session.query(Invoice).get(id)
+
+        return invoice
+
+class InvoiceItem(Base):
+
+    __tablename__ = 'invoice_items'
+
+    id = Column(Integer, primary_key=True, unique=True)
+    invoice_id = Column(Integer, ForeignKey('invoices.id'))
+    units = Column(Integer)
+    description = Column(String(64))
+    amount = Column(Numeric)
+
+    def create(invoice_id, units, description, amount):
+
+        engine = create_engine('mysql://root@localhost/test')
+        Base.metadata.create_all(bind=engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        invoice_item = InvoiceItem(
+            invoice_id=invoice_id,
+            units=units,
+            description=description,
+            amount=amount
+        )
+
+        session.add(invoice_item)
+        session.commit()
+
+        return invoice_id
+
+    def find(id):
+
+        engine = create_engine('mysql://root@localhost/test')
+        Base.metadata.create_all(bind=engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        invoice_items = session.query(InvoiceItem).filter(
+            InvoiceItem.invoice_id==id
+        ).all()
+
+        return invoice_items
